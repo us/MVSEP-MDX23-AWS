a7a0f19428128e0cccc99635a4f2e1582b89cc0e
syny <rahmetsaritekin@gmail.com>
2024-02-28 21:28:59 +0300

    feat: transform_fn

--- code/inference.py
+++ code/inference.py
@@ -1,33 +1,15 @@
 import base64
 import json
 import logging
-
 import librosa
 import numpy as np
 from main import EnsembleDemucsMDXMusicSeparationModel
-
 import io
 import soundfile as sf
 
 # Initialize logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  # Set to DEBUG for development, INFO for production
-
-# # Default options for model prediction
-# default_options = {
-#     "overlap_demucs": 0.1,
-#     "overlap_VOCFT": 0.1,
-#     "overlap_VitLarge": 1,
-#     "overlap_InstVoc": 1,
-#     "weight_InstVoc": 8g,
-#     "weight_VOCFT": 1,
-#     "weight_VitLarge": 5,
-#     "large_gpu": True,
-#     "BigShifts": 7,
-#     "vocals_only": False,
-#     "use_VOCFT": False,
-#     "output_format": "FLOAT",
-# }
+logger.setLevel(logging.DEBUG)  # Adjust logging level as needed
 
 def model_fn(model_dir):
     """Load the ensemble model for music separation."""
@@ -35,106 +17,71 @@ def model_fn(model_dir):
     model = EnsembleDemucsMDXMusicSeparationModel(model_dir=model_dir)
     return model
 
-
-def input_fn(request_body, request_content_type):
-    """Preprocess incoming audio data and additional parameters before prediction."""
-    logger.info('Received request_body: %s', request_body[:100])  # Print first 100 chars for debugging
-    logger.info('Received request_content_type: %s', request_content_type)
+def transform_fn(model, request_body, request_content_type, response_content_type):
+    """
+    Function to preprocess request, perform prediction, and postprocess before sending response.
+    """
+    logger.info('Transforming input for prediction')
     
-    # Ensure the correct content type is being used
+    # Preprocessing
     if request_content_type != 'application/json':
         logger.error('Unsupported content type: %s', request_content_type)
         raise ValueError(f'Unsupported content type: {request_content_type}')
-    
+
     try:
-        # Parse the JSON body to extract audio data and parameters
-        input_data = json.loads(request_body)
-        logger.info('Parsed input data successfully.', input_data)
-        
-        # Additional debugging to ensure input_data is as expected
-        if not isinstance(input_data, dict):
-            logger.error('Parsed input data is not a dictionary. Actual type: %s', type(input_data, "\n", input_data))
-            raise ValueError('Parsed input data is not a dictionary.')
-        
-        # Decode the base64-encoded audio data
+        # Parse JSON body to extract audio data and parameters
+        input_data = json.loads(io.BytesIO(request_body))
         audio_data_base64 = input_data['audio']
         audio_data = base64.b64decode(audio_data_base64)
         audio_buffer = io.BytesIO(audio_data)
         
-        # Load the audio with librosa
+        # Load audio with librosa
         audio, sample_rate = librosa.load(audio_buffer, mono=False, sr=44100)
         if len(audio.shape) == 1:
             audio = np.stack([audio, audio], axis=0)
-        
-        # Combine audio data and additional parameters in the returned dictionary
-        return {
+
+        # Combine audio data and additional parameters
+        preprocessed_data = {
             'audio': audio, 
             'sr': sample_rate,
             'options': input_data.get('options', {
-                                        "overlap_demucs": 0.1,
-                                        "overlap_VOCFT": 0.1,
-                                        "overlap_VitLarge": 1,
-                                        "overlap_InstVoc": 1,
-                                        "weight_InstVoc": 8,
-                                        "weight_VOCFT": 1,
-                                        "weight_VitLarge": 5,
-                                        "large_gpu": True,
-                                        "BigShifts": 7,
-                                        "vocals_only": False,
-                                        "use_VOCFT": False,
-                                        "output_format": "FLOAT",
-                                    })
+                   "overlap_demucs": 0.1,
+                    "overlap_VOCFT": 0.1,
+                    "overlap_VitLarge": 1,
+                    "overlap_InstVoc": 1,
+                    "weight_InstVoc": 8,
+                    "weight_VOCFT": 1,
+                    "weight_VitLarge": 5,
+                    "large_gpu": True,
+                    "BigShifts": 7,
+                    "vocals_only": False,
+                    "use_VOCFT": False,
+            })
         }
     except Exception as e:
-        logger.error(f"Error processing input data: {e},\nrequest_body: {request_body}, request_content_type: {request_content_type}\n input_data: {input_data}")
+        logger.error(f"Error processing input data: {e}")
         raise
 
-
-def predict_fn(input_data, model):
-    """Run prediction on preprocessed audio data."""
-    logger.info('Performing separation on audio data')
-    audio, sample_rate, options = input_data['audio'], input_data['sr'], input_data['options']
-    result, sample_rates, instruments = model.separate_music_file(audio.T, sample_rate, options)
+    # Prediction
+    logger.info('Performing prediction')
+    result, sample_rates, instruments = model.separate_music_file(preprocessed_data['audio'].T, preprocessed_data['sr'], preprocessed_data['options'])
     
-    return result, sample_rates, instruments, options
-
-
-def output_fn(prediction, accept='application/octet-stream'):
-    """
-    Process the prediction output to audio buffers and store them in a dictionary.
-    """
-    result, sample_rates, instruments, options = prediction
+    # Postprocessing
+    logger.info('Postprocessing prediction output')
     audio_buffers = {}
-    
     for instrum in instruments:
-        output_name = '_{}.wav'.format(instrum)
+        output_name = f'_{instrum}.wav'
         buffer = io.BytesIO()
         sf.write(buffer, result[instrum], sample_rates[instrum], format='WAV')
         buffer.seek(0)
-        audio_buffers[output_name] = buffer
+        audio_buffers[output_name] = buffer.getvalue()  # Modified to getvalue for binary data
 
-    # Instrumental part 1
-    if 'instrum' in result:
-        inst = result['instrum']
-        output_name = '_instrum.wav'
-        buffer = io.BytesIO()
-        sf.write(buffer, inst, sample_rates.get('instrum', 44100), format='WAV')
-        buffer.seek(0)
-        audio_buffers[output_name] = buffer
-
-    # Instrumental part 2, if vocals_only option is False
-    if not options['vocals_only'] and all(x in result for x in ['bass', 'drums', 'other']):
-        inst2 = result['bass'] + result['drums'] + result['other']
-        output_name = '_instrum2.wav'
-        buffer = io.BytesIO()
-        sf.write(buffer, inst2, sample_rates.get('bass', 44100), format='WAV')  # Assuming same SR for all
-        buffer.seek(0)
-        audio_buffers[output_name] = buffer
-
-    if accept == 'application/octet-stream':
-        # This example simply returns the dictionary of buffers
-        # Adapt this as needed for your use case, e.g., packaging multiple files, handling accept types
-        return audio_buffers
+    # Example for one instrument, similar logic for others as needed
+    # Adapt this part based on how you want to package and return the audio buffers
+    
+    if response_content_type == 'application/octet-stream':
+        # Assuming returning binary data for one instrument for simplicity
+        return audio_buffers[next(iter(audio_buffers))], response_content_type
     else:
-        raise ValueError(f"Unsupported accept header: {accept}")
+        raise ValueError(f"Unsupported response content type: {response_content_type}")
 
